5aadd39fe96afc7105f74f120d6bb3fb46403b23
Mamed766 <mehemmeddemirli57@gmail.com>
2024-08-31 16:14:43 +0400

    feat: loading created

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { ReactElement, useState } from "react";
 import { FaPlay } from "react-icons/fa";
 import { motion } from "framer-motion";
 import RippleButton from "../_components/Buttons/RippleButton";
@@ -19,7 +19,7 @@ import CustomerFeedback from "../_components/CustomerFeedback/CustomerFeedback";
 import Consultation from "../_components/Consultation/Consultation";
 import News from "../_components/News/News";
 
-const HomePage: React.FC = () => {
+export default function HomePage(): ReactElement {
   return (
     <div className="">
       <div className="bg-[#F0F4FF] pt-[10rem] pb-[15rem]">
@@ -65,6 +65,4 @@ const HomePage: React.FC = () => {
       </div>
     </div>
   );
-};
-
-export default HomePage;
+}
