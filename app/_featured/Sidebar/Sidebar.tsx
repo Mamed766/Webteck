bda8a82a3d88b7a5867c50dcfcd9fea2753b10f0
Mamed766 <mehemmeddemirli57@gmail.com>
2024-09-01 21:11:56 +0400

    refactor: sidebar

--- app/_featured/Sidebar/Sidebar.tsx
+++ app/_featured/Sidebar/Sidebar.tsx
@@ -79,6 +79,18 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
             </div>
 
             <div
+              role="button"
+              className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+            >
+              <div className="grid place-items-center mr-4">
+                <FaAngleRight />
+              </div>
+              <Link className="w-full py-3" href="/cart">
+                Cart
+              </Link>
+            </div>
+
+            <div
               onClick={() => {
                 handleLogout();
                 onClose();
